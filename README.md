6d718fe084831d7271a95ccfb6744d460e12489f
wrac4242 <wrac4242@gmail.com>
2021-04-17 21:03:29 +0100

    updated readme, basic layout of propogation functions

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 an AI that will check if something is a blob emoji or not  
 I wont push that much to the repo, its just here if needed  
 using emojis from the Blob Emojis discord server  
-more info [here](https://blobs.gg/)
+more info [here](https://blobs.gg/)  
+uses code/references https://pwy.io/en/posts/learning-to-fly-pt1/
--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -12,25 +12,40 @@ struct Neuron {
 }
 
 impl Network {
-    pub fn propagate(&self, mut input: Vec<f32>) -> Vec<f32> {
-        todo!();
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
         //send the input to the next layer, where it processes it, after looping through all layers, return input
+        for layer in &self.layers {
+            inputs = layer.propagate(inputs);
+        }
 
-        input
+        inputs
     }
 }
 
 impl Layer {
-    fn propagate(&self, mut input: Vec<f32>) -> Vec<f32> {
-        todo!();
+    fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
         //loops over each neuron in the layer, gives it the input, and expects a f32 from it
+        let mut outputs = Vec::with_capacity(self.neurons.len());
+
+        for neuron in &self.neurons {
+            let output = neuron.propagate(&inputs);
+            outputs.push(output);
+        }
+        outputs
     }
 }
 
 impl Neuron {
-    fn propagate(&self, mut input: Vec<f32>) -> f32 {
-        todo!();
+    fn propagate(&self, inputs: &[f32]) -> f32 {
         //loops over all the input values, multiplies it by the given weight, and then adds bias
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+
     }
 }
 
